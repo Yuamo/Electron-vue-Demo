c106504f1b8cd274ed714ec30087319cba0e6196
yumao <hansanye@126.com>
2017-06-29 16:42:29 +0800

    加入element-ui ，iview

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,8 +4,19 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
-
+/* 重置全局样式 */
 import '../../static/css/reset.scss'
+/* 引入element-ui */
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+
+/* 引入iview */
+import iView from 'iview'
+import 'iview/dist/styles/iview.css' // 使用 CSS
+
+Vue.use(ElementUI)
+
+Vue.use(iView)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
